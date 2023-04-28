f3983b3c247c40ae294192f6b8523002ce1992e3
hardinger-kirin <khardinger@nevada.unr.edu>
2023-04-28 16:14:37 -0700

    Testing workflow

--- Tests.py
+++ Tests.py
@@ -1,10 +1,12 @@
 import trainer
-import pokemon
-import move
 
 import unittest
 class TestMethods(unittest.TestCase):
-    pass
+    #UNIT TESTS FOR TRAINER CLASS *************************
+    def test_get_trainer_name(self):
+        test_name = "Ash"
+        test_trainer = trainer.Trainer(test_name)
+        self.assertEqual(test_trainer.get_name(), test_name)
 
 if __name__ == '__main__':
     unittest.main()
